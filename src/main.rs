0b00974905385fcc2b0863218d8f3f8b1a761d1f
TilakMaddy <tilak.madichetti@gmail.com>
2023-03-19 19:58:35 +0530

    star

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::ptr::replace;
+use anyhow::Result;
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -38,19 +40,92 @@ fn handle_connection(mut stream: TcpStream) {
 
 fn craft_response(request_string: String) -> String {
 
-    let decomposed = request_string.split_once(" ");
-    let default_response = "+PONG\r\n".to_string();
+    let mut decomposed: Vec<&str> = request_string.split("\r\n").collect();
+    decomposed.pop(); // Getting rid of the blank "" due to the last CR-LF
 
-    if let Some(message) = decomposed {
-        if message.1.len() == 0 {
-
-            return default_response;
+    let op: Operation = decomposed.into();
+    return match op {
+        Operation::Pong(response_data) => {
+            response_data.join("\r\n")
         }
-        let size = message.1.as_bytes().len();
-        let response = format!("${}\r\n{}\r\n", size, message.1);
-        return response;
     }
 
-    return default_response;
+}
+
+#[derive(PartialEq, Debug)]
+enum Operation {
+    Pong(Vec<String>)
+}
+
+impl From<Vec<&str>> for Operation {
+
+    fn from(mut data: Vec<&str>) -> Self {
+        if data == &["*1", "$4", "ping"] {
+            return Operation::Pong(vec![
+                "*1".to_string(),
+                "$4".to_string(),
+                "pong".to_string()
+            ]);
+        }
+
+        let mut d = data[0].to_string();
+        let data_len: String = d.drain(1..d.len()).collect();
+        let data_len = data_len.parse::<usize>().unwrap();
+
+        let mut response_data = vec![];
+        response_data.push(format!("*{}", data_len - 1));
+        let rest: Vec<_> = data.drain(3..data.len()).collect();
+
+        for elem in rest {
+            response_data.push(elem.to_string());
+        }
+
+        Operation::Pong(response_data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Operation;
+
+    #[test]
+    fn check_decomposition_only_ping() {
+        let req = "*1\r\n$4\r\nping\r\n";
+
+        let mut decomposed: Vec<&str> = req.split("\r\n").collect();
+        assert_eq!(decomposed, &["*1", "$4", "ping", ""]);
+
+        decomposed.pop();
+        assert_eq!(decomposed, &["*1", "$4", "ping"]);
+
+        let op: Operation = decomposed.into();
+        assert_eq!(op, Operation::Pong(vec![
+                "*1".to_string(),
+                "$4".to_string(),
+                "pong".to_string()
+            ])
+        );
+    }
+
+    #[test]
+    fn check_decomposition_ping_with_words() {
+
+        let mut decomposed: Vec<&str> = vec![
+            "*2", "$4", "ping", "$3", "cat"
+        ];
+
+        let op: Operation = decomposed.into();
+        assert_eq!(op, Operation::Pong(
+            vec![
+                "*1".to_string(),
+                "$3".to_string(),
+                "cat".to_string()
+            ]
+        ));
+
+    }
+
+
+
 }
 
