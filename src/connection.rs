3d892a29eeb438c0326b155d04247dc4f6fac6aa
TilakMaddy <tilak.madichetti@gmail.com>
2023-03-24 17:35:39 +0530

    added Set Get

--- src/connection.rs
+++ src/connection.rs
@@ -2,6 +2,7 @@ use std::io::Cursor;
 use bytes::{Buf, BytesMut};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
 use tokio::net::TcpStream;
+use crate::Db;
 use crate::frame::Frame;
 
 pub struct Connection {
@@ -18,7 +19,7 @@ impl Connection {
         return redis_conn;
     }
 
-    pub async fn run_read_write_loop(&mut self) {
+    pub async fn run_read_write_loop(&mut self, db: Db) {
         loop {
             let read_bytes = self.stream.read_buf(&mut self.buffer).await
                 .expect("failed to read bytes into buffer ");
@@ -30,7 +31,7 @@ impl Connection {
             let mut buf = Cursor::new(&self.buffer[..]);
             let frame =  Frame::parse(&mut buf);
             println!("Received {:?}", frame);
-            send_response(frame, self).await;
+            send_response(frame, self, db.clone()).await;
 
             self.buffer.advance(read_bytes);
         }
@@ -38,20 +39,54 @@ impl Connection {
 
 }
 
-async fn send_response(frame: Frame, conn: &mut Connection) {
+async fn send_response(frame: Frame, conn: &mut Connection, db: Db) {
+
+    let mut bytes_to_write:Vec<Vec<u8>> = vec![];
+
     if let Frame::Array(items) = frame {
         let args: Vec<_> = items.clone().into_iter().skip(1).collect();
         if let Frame::Bulk(cmd_bytes) = items.get(0).unwrap() {
             let cmd = String::from_utf8(cmd_bytes.to_vec()).unwrap();
             match cmd.to_ascii_lowercase().as_ref() {
                 "ping" => {
-                    let _ = conn.stream.write(b"+PONG\r\n").await.unwrap();
-                    let _ = conn.stream.flush().await;
+                    bytes_to_write.push(b"+PONG\r\n".to_vec());
                 }
                 "echo" => {
                     let write_data = args.first().unwrap().clone().encode();
-                    conn.stream.write(write_data.as_bytes()).await.unwrap();
-                    let _ = conn.stream.flush().await;
+                    bytes_to_write.push(write_data.as_bytes().to_vec());
+                }
+                "set" => {
+                    let key = args.get(0).unwrap().clone();
+                    let value = args.get(1).unwrap().clone();
+                    if let Frame::Bulk(key_bytes) = key {
+                        if let Frame::Bulk(value_bytes) = value {
+                            let key_str = String::from_utf8(key_bytes.to_vec()).unwrap();
+                            {
+                                let mut db = db.lock().unwrap();
+                                db.insert(key_str, value_bytes);
+                            }
+                            bytes_to_write.push(b"+OK\r\n".to_vec());
+                        }
+                    }
+                    else { panic!("invalid protocol format !") }
+                }
+                "get" => {
+                    let key = args.get(0).unwrap().clone();
+                    if let Frame::Bulk(key_bytes) = key {
+                        let key_str = String::from_utf8(key_bytes.to_vec()).unwrap();
+                        let db = db.lock().unwrap();
+
+                        if let Some(val) = db.get(key_str.as_str()).clone() {
+                            let len = val.len();
+                            let ret_val = format!("${}\r\n", len);
+                            bytes_to_write.push(ret_val.as_bytes().to_vec());
+                            bytes_to_write.push(val.to_vec());
+                            bytes_to_write.push(b"\r\n".to_vec());
+                        } else {
+                            bytes_to_write.push(b"-1\r\n".to_vec());
+                        }
+                    }
+                    else { panic!("invalid protocol format !") }
                 }
                 _ => panic!("command not implemented ")
             }
@@ -59,4 +94,10 @@ async fn send_response(frame: Frame, conn: &mut Connection) {
         else { panic!("invalid protocol format !"); }
     }
     else { panic!("Not ready !") }
+
+    // write response to the connection stream
+    for group in bytes_to_write {
+        conn.stream.write_all(group.as_ref()).await.unwrap();
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use bytes::Bytes;
 use tokio::net::{TcpListener, TcpStream};
 use crate::config::redis_defaults;
 use crate::connection::Connection;
@@ -6,6 +9,8 @@ mod config;
 mod connection;
 mod frame;
 
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind(redis_defaults()).await
@@ -13,17 +18,21 @@ async fn main() {
 
     println!("Listening for incoming connections on {}", redis_defaults());
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (stream, _) = listener.accept().await
             .expect("Failed to accept client's request ");
 
+        let db = db.clone();
+
         tokio::spawn(async move {
-            process_client(stream).await;
+            process_client(stream, db).await;
         });
     }
 }
 
-async fn process_client(stream: TcpStream) {
+async fn process_client(stream: TcpStream, db: Db) {
     let mut conn = Connection::new(stream);
-    conn.run_read_write_loop().await;
+    conn.run_read_write_loop(db.clone()).await;
 }
